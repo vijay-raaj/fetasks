c6951f9855db170d2e8567edf8393bafc8ca6257
Hersheni <hersheni.r@sptr.co>
2023-08-04 18:26:27 +0530

    login added

--- fetasks/src/router/index.js
+++ fetasks/src/router/index.js
@@ -50,4 +50,4 @@ const router = createRouter({
   routes
 })
 
-export default router
+export default router;
--- fetasks/src/views/SignUp.vue
+++ fetasks/src/views/SignUp.vue
@@ -1,10 +1,21 @@
 <template>
-    <div>
+    <div class="login">
+        <h1>LOGIN</h1>
         <form @submit.prevent="createPost">
-            Enter the Name:<input type="text" v-model="mydata.name"><br> <br><br>
-            Enter the Email:<input type="text" v-model="mydata.email"><br><br><br>
-            <button type="submit"> Post</button>
+            <div class="user">
+                <label for="name">Name</label>
+            
+        <input type="text" id="name" v-model="mydata.name" required />
+      <br>
+    <br>
+        <label for="email">Email</label>
+        <input type="email" id="email" v-model="mydata.email" required />
+            </div>
+            <br>
+            
+            <button type="submit">login</button>
         </form> 
+        
             </div>
   </template>
   
@@ -33,4 +44,54 @@
     }
   }
   </script>
+  <style>
+
+  h1{
+    padding: 3%;
+    color:white;
+
+  }
+  .login {
+    background-image: url("@/assets/login.jpg");
+    height: 100vh;
+    background-size: cover;
+    background-repeat: no-repeat;
+    
+    
+  }
   
+  .user {
+    color:black;
+    font:black ;
+    margin-bottom: 4px;
+  }
+  
+  label {
+    display: block;
+    margin-bottom: 5px;
+    color:white;
+    
+  }
+  
+  input {
+    width: 25%;
+    padding: 8px 8px;
+    font-size: 16px;
+    color:black;
+  }
+  
+  button {
+    background-color: #4caf50;
+    color: white;
+    padding: 10px 15px;
+    border: none;
+    cursor: pointer;
+    font-size: 16px;
+  }
+  @media only screen and (max-width:600px) {
+    input {
+        width:70%
+    }
+    
+  }
+  </style>
